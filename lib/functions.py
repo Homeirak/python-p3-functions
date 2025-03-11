e97a5226a4f16faa5af373e761128435bc849a39
homeira kalim <homeirakalim@gmail.com>
2025-03-10 20:00:46 -0400

    python intro to functions lab complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
+# def my_function(param):
+#     print("Running my_function")
+#     return param + 1
+
+# The return keyword will disrupt the execution of your function, and prevent Python from running any lines of code after the return keyword.
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number/2
